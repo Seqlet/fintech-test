02d13c8a450ac0c971794b7b75dd239946bfff02
Seqlet <ratchanoneks@gmail.com>
2021-09-21 10:40:44 +0700

    feat: React web for candlestick

---
+++ frontend/src/Components/Candlestick.js
@@ -0,0 +1,67 @@
+import Chart from "react-google-charts";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { format } from "date-fns";
+
+const Candlestick = ({ symbol }) => {
+  const [bars, setBars] = useState([]);
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    const getData = async () => {
+      await axios
+        .get(`http://127.0.0.1:8000/get-candle/${symbol}`, {
+          cancelToken: source.token,
+        })
+        .then((respond) => {
+          setBars(respond.data.data);
+        })
+        .catch((err) => {
+          if (axios.isCancel(err)) {
+            console.log("Request Cancel");
+          } else {
+            console.log(err);
+          }
+        });
+    };
+    getData();
+
+    return () => {
+      source.cancel();
+    };
+  }, [symbol]);
+
+  //for formating the data for the react-google-charts-library
+  const dataFormat = bars.map((stick) => {
+    return [
+      format(new Date(stick.timestamp), "yyyy-MM-dd hh:mm:ss aa"),
+      parseFloat(stick.low),
+      parseFloat(stick.open),
+      parseFloat(stick.close),
+      parseFloat(stick.high),
+    ];
+  });
+
+  dataFormat.unshift(["day", "a", "b", "c", "d"]); //for the first row of react-google-charts
+
+  return (
+    <div>
+      <Chart
+        width={"100%"}
+        height={350}
+        chartType="CandlestickChart"
+        loader={<div>Loading Chart</div>}
+        data={dataFormat}
+        options={{
+          legend: "none",
+          bar: { groupWidth: "100%" }, // Remove space between bars.
+          candlestick: {
+            fallingColor: { strokeWidth: 0, fill: "#a52714" }, // red
+            risingColor: { strokeWidth: 0, fill: "#0f9d58" }, // green
+          },
+        }}
+      />
+    </div>
+  );
+};
+
+export default Candlestick;
